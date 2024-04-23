845ec9992e94c3eddfe4b8064611b2a5ce7697d0
Vasyl Protsukovych <protcykovich@gmail.com>
2024-04-24 00:18:56 +0200

    Implemented task 1,2,3

---
+++ README.md
@@ -0,0 +1,2 @@
+All js files are stored in "js" folder and already connected to the index.html.
+Plz, check browser console to see the output.
---
+++ js/task-2.js
@@ -0,0 +1,32 @@
+class Storage {
+  #items;
+
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    /* Very simple form of case sensivity ignore */
+    const caseInsensitiveItem = itemToRemove.toLowerCase();
+    this.#items = this.#items.filter((item) => item.toLowerCase() !== caseInsensitiveItem);
+  }
+}
+
+console.log("Task 2:\n");
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+console.log("\n");
